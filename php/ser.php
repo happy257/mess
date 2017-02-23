44345c85956529e0afa6513d74d45b8355f9212a
Abhinav Sachdeva <happy257@gmail.com>
2017-02-23 19:51:28 +0530

    Create ser.php

---
+++ php/ser.php
@@ -0,0 +1,6 @@
+<?php
+	sleep(0.5);
+	$res = array("name"=>"Abhinav", "birthdate"=>"21-02");
+	echo json_encode($res);
+	//echo "[{type:'notification',cat:'late',from:'Abhinav',desc:'short description here'},]";
+?>
